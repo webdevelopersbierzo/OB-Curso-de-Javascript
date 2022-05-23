9c02430703331dfcee1290bcd22d6052b2fe5a76
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2022-05-23 12:19:02 +0200

    fin ejercicio 2

---
+++ ejercicio2/ejercicio2.js
@@ -0,0 +1,10 @@
+let name =" Mateo";
+let age = 45;
+let developers = true;
+let dateBirth = "1976-12-26";
+const bookfavorite = {
+    title : "La catedral del mar",
+    author: "Idelfonso falcones",
+    datePublication: "2011-1-11",
+    url : "https://www.amazon.es/catedral-del-mar-BEST-SELLER/dp/849908804X"
+}
