684236a7dbbad25f1a7e67708b7ed9dfd4248be9
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2022-05-31 12:14:36 +0200

    End exercise 5

---
+++ ejercicio5/ejercicio5.js
@@ -0,0 +1,19 @@
+/** 
+ * @file: ejercicio5.js
+ * @author: @webdevelopersbierzo
+ * @description: exercise 5 of the Openbootcamp javascript course
+*/
+
+let heightCm = 170;
+let heightMeters= 1.68;
+let weight= 84.92;
+// Round up
+let heightMeterRoundUp = heightMeters.toFixed(1)
+console.log(heightMeterRoundUp)
+// Round down
+let weightKgRoundDown = weight.toFixed(1)
+console.log(weightKgRoundDown)
+
+// max valor
+let max_valor_Js = ((Number.MAX_VALUE + 1) == Number.MAX_VALUE);
+console.log(max_valor_Js)
