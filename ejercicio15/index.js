59dab44bddc0a1ea1a9bb327fb0cb6a2bd232236
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2022-06-23 08:33:28 +0200

    fin ejercicio 15

---
+++ ejercicio15/index.js
@@ -0,0 +1,12 @@
+
+let name = "Oscar"
+let surname = "Corral"
+
+const fullName = {
+    name : "Oscar",
+    surname: "Corral"
+}
+
+//sessionStorage.setItem("fullName", JSON.stringify(fullName))
+//localStorage.setItem("fullName", JSON.stringify(fullName))
+//document.cookie = JSON.stringify(fullName);
